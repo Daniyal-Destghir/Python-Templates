225335097eb4115f0d53c99129d47ff4e8531d22
Hacker-Boy9999 <fordaniyal@outlook.com>
2021-06-09 14:42:52 +0300

    Create Creating a Small Adventure Game.py

---
+++ templates/Creating a Small Adventure Game.py
@@ -0,0 +1,4 @@
+#WARNING: WHEN WRITING THESE TEXTS I ALWAYS STARTED WITH "#" by doing these it means that python will not read this, so when adding code, Please dont add "#".
+
+# Did you ever want to create a small adventure game?
+# Well, today I am going to show you how!
